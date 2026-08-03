9980841aeae1eaf9826ae35823c7386395bf1385
Cyrille <avantpost@gmail.com>
2026-08-03 16:47:56 +0200

    Update enigme1Url to point to enigme1.html

--- escapegame_digital/js/config.js
+++ escapegame_digital/js/config.js
@@ -2,6 +2,6 @@ window.REBOND_CONFIG = {
   supabaseUrl: "https://tdsrqmpfdvntybdlvejy.supabase.co",
   supabaseKey: "sb_publishable_MIYryZDKmU4cGMIGdXojnA_GQ5qmpOO",
 
-  enigme1Url: "../intro.html",
+  enigme1Url: "../enigme1.html",
   diplomaUrl: "../diplome/diplome.html"
 };
